dc4e5497eac9ab14988a50fecff1e7d5ccf78037
Luca <lu.cucchi@gmail.com>
2020-03-05 14:23:47 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -8,10 +8,9 @@ $('#invia').click(function(){
     $('.sent').append(messaggio); // creo cascata messaggi nella main chat
 
 setTimeout(function(){
-    var risposta = $('.template2 .speech-bubble-received').clone(); //copia contenuto messaggio inviato che è dentro il template (che è display none)
-    risposta.children('.testo-bubble-received').text(messaggioInput); //modif. testo messaggio inviato che e inserimento nel bubble
+    var risposta = $('.template2 .speech-bubble-received').clone(); //copia contenuto rispostao che è dentro il template (che è display none)
+    risposta.children('.testo-bubble-received').text(messaggioInput); //modif. testo risposta che e inserimento nel bubble
     risposta.children('.time-received').text('05:05'); //aggiungo ora
     $('.received').append(risposta); // creo cascata messaggi nella main chat
 }, 1000)
-
 })
