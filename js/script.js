7bde55ffe02ad1ad103dcda787e7a46cb7d2b3c9
Luca <lu.cucchi@gmail.com>
2020-03-05 14:12:18 +0100

    ok milestone 1b (necessario fix)

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ $('#invia').click(function(){
     messaggio.children('.testo-bubble-sent').text(messaggioInput); //modif. testo messaggio inviato che e inserimento nel bubble
     messaggio.children('.time-sent').text('05:05'); //aggiungo ora
     $('.sent').append(messaggio); // creo cascata messaggi nella main chat
+
+setTimeout(function(){
+    var risposta = $('.template2 .speech-bubble-received').clone(); //copia contenuto messaggio inviato che è dentro il template (che è display none)
+    risposta.children('.testo-bubble-received').text(messaggioInput); //modif. testo messaggio inviato che e inserimento nel bubble
+    risposta.children('.time-received').text('05:05'); //aggiungo ora
+    $('.received').append(risposta); // creo cascata messaggi nella main chat
+}, 1000)
+
 })
