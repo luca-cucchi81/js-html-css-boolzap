105d40c2086a0225a44dc9f1aed3ac60cf5e5943
Luca <lu.cucchi@gmail.com>
2020-03-05 17:06:18 +0100

    ok milestone 2b (errori su 2a)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $('#invia').click(function(){
     var messaggioInput = $('#messaggio').val();
     $('#messaggio').val('');
@@ -5,12 +6,25 @@ $('#invia').click(function(){
     var messaggio = $('.template .speech-bubble-sent').clone(); //copia contenuto messaggio inviato che è dentro il template (che è display none)
     messaggio.children('.testo-bubble-sent').text(messaggioInput); //modif. testo messaggio inviato che e inserimento nel bubble
     messaggio.children('.time-sent').text('05:05'); //aggiungo ora
-    $('.sent').append(messaggio); // creo cascata messaggi nella main chat
+    $('.main-chat').append(messaggio); // creo cascata messaggi nella main chat
 
 setTimeout(function(){
     var risposta = $('.template2 .speech-bubble-received').clone(); //copia contenuto rispostao che è dentro il template (che è display none)
-    risposta.children('.testo-bubble-received').text(messaggioInput); //modif. testo risposta che e inserimento nel bubble
+    risposta.children('.testo-bubble-received').text('OK!!'); //modif. testo risposta che e inserimento nel bubble
     risposta.children('.time-received').text('05:05'); //aggiungo ora
-    $('.received').append(risposta); // creo cascata messaggi nella main chat
+    $('.main-chat').append(risposta); // creo cascata messaggi nella main chat
 }, 1000)
+});
+
+
+$('#cerca-contatti').keyup(function(event){
+    var filtro = $(this).val().toLowerCase();
+    $('.text-user h3').each(function(){ // Se nella lista contatti è presente il carattere digitato visualizzarlo
+        // console.log($(this).text());
+        if ($(this).text().toLowerCase().includes(filtro)) { // Se il nome del list item ha al suo interno i caratteri digitati visualizzalo
+            $(this).parents('.utente').show();
+        } else { // Altrimenti non visualizzarlo
+            $(this).parents('.utente').hide();
+        }
+    });
 })
