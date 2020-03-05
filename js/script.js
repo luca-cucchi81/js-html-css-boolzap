29dd07b42b60800a9fd635a17efa79a7951a421f
Luca <lu.cucchi@gmail.com>
2020-03-05 17:19:18 +0100

    fix milestone 2a (esercizio odierno completo)

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ $('#invia').click(function(){
     var messaggioInput = $('#messaggio').val();
     $('#messaggio').val('');
 
-    var messaggio = $('.template .speech-bubble-sent').clone(); //copia contenuto messaggio inviato che è dentro il template (che è display none)
+    var messaggio = $('.box-sent .template').clone(); //copia contenuto messaggio inviato che è dentro il template (che è display none)
     messaggio.children('.testo-bubble-sent').text(messaggioInput); //modif. testo messaggio inviato che e inserimento nel bubble
     messaggio.children('.time-sent').text('05:05'); //aggiungo ora
     $('.main-chat').append(messaggio); // creo cascata messaggi nella main chat
 
 setTimeout(function(){
-    var risposta = $('.template2 .speech-bubble-received').clone(); //copia contenuto rispostao che è dentro il template (che è display none)
+    var risposta = $('.box-received .template2').clone(); //copia contenuto rispostao che è dentro il template (che è display none)
     risposta.children('.testo-bubble-received').text('OK!!'); //modif. testo risposta che e inserimento nel bubble
     risposta.children('.time-received').text('05:05'); //aggiungo ora
     $('.main-chat').append(risposta); // creo cascata messaggi nella main chat
